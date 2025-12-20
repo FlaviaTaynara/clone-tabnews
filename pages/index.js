a4ae671a437796f48be26beef542715a63ff41aa
Flavia Maciulevicius <80327714+FlaviaTaynara@users.noreply.github.com>
2025-12-20 18:14:49 +0000

    test

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+function Home() {
+  return (
+    <>
+      <h1>Rūt 1:16</h1>
+      <h3>Kur tu eisi, ir aš eisiu;</h3>
+      <h3>kur tu gyvensi, ir aš gyvensiu.</h3>
+      <h3>Tavo tauta yra mano tauta, ir tavo Dievas – mano Dievas.</h3>
+    </>
+  );
+}
+
+export default Home;
